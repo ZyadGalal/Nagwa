a4364a342d0438c54f6ee2565509880016998a88
ZyadGalal <dev_zyad_galal@yahoo.com>
2022-03-27 21:29:16 +0200

    Initial Commit

---
+++ NagwaTask/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NagwaTask
+//
+//  Created by Zyad Galal on 27/03/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
