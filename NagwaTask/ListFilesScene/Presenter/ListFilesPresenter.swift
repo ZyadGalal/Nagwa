1304c3816b6623212762b8d49f7cd609b171fde2
ZyadGalal <dev_zyad_galal@yahoo.com>
2022-03-28 21:59:10 +0200

    implement list file functions , data source functions

--- NagwaTask/ListFilesScene/Presenter/ListFilesPresenter.swift
+++ NagwaTask/ListFilesScene/Presenter/ListFilesPresenter.swift
@@ -7,13 +7,16 @@
 
 
 import Foundation
+import AVFoundation
 
 protocol ListFilesView: AnyObject{
     var presenter: ListFilesPresenterProtocol? {get set}
 }
 
 protocol ListFilesPresenterProtocol: AnyObject {
-    
+    func viewDidLoad()
+    func numberOfFiles() -> Int
+    func getFile(at index: Int) -> FileModel
 }
 
 class ListFilesPresenter: ListFilesPresenterProtocol{
@@ -21,9 +24,54 @@ class ListFilesPresenter: ListFilesPresenterProtocol{
     private weak var view: ListFilesView?
     private var router: ListFilesRouter
     private var interactor: ListFilesInteractor
-    init (view: ListFilesView, router: ListFilesRouter, interactor: ListFilesInteractor){
+    private var files: [FileModel] = []
+    private var directory: String = ""
+    private let formatter: DateComponentsFormatter = {
+        let formatter = DateComponentsFormatter()
+        formatter.unitsStyle = .positional
+        formatter.zeroFormattingBehavior = .pad
+        formatter.allowedUnits = [.hour, .minute, .second]
+        return formatter
+    }()
+    
+    init (view: ListFilesView, router: ListFilesRouter, interactor: ListFilesInteractor, directory: String){
         self.view = view
         self.router = router
         self.interactor = interactor
+        self.directory = directory
     }
+    func viewDidLoad() {
+        files = listFilesFrom(directory: directory, with: "mp3")
+    }
+    func listFilesFrom(directory: String, with extensionWanted: String) -> [FileModel] {
+
+        let documentURL = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first!
+        let Path = documentURL.appendingPathComponent(directory).absoluteURL
+
+        do {
+            let directoryContents = try FileManager.default.contentsOfDirectory(at: Path, includingPropertiesForKeys: nil, options: [])
+
+            let filesPath = directoryContents.filter{ $0.pathExtension == extensionWanted || $0.hasDirectoryPath}
+            let files = filesPath.map{
+                FileModel(fileName: $0.lastPathComponent,
+                          path: $0,
+                          fileType: $0.pathExtension == extensionWanted ? .Audio : .Folder,
+                          duration: formatter.string(from: AVAsset(url: $0).duration.seconds) ?? "0") }
+            
+            return files
+
+        } catch {
+            print(error.localizedDescription)
+        }
+        return []
+    }
+    
+    func numberOfFiles() -> Int {
+        return files.count
+    }
+    
+    func getFile(at index: Int) -> FileModel{
+        return files[index]
+    }
+    
 }
