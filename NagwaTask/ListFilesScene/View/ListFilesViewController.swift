7f7ef4a591e1f9e6e1c798ed324198a937040c4f
ZyadGalal <dev_zyad_galal@yahoo.com>
2022-03-28 21:57:28 +0200

    setup tableView

--- NagwaTask/ListFilesScene/View/ListFilesViewController.swift
+++ NagwaTask/ListFilesScene/View/ListFilesViewController.swift
@@ -13,12 +13,40 @@ class ListFilesViewController: UIViewController {
     var presenter: ListFilesPresenterProtocol?
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        setupUI()
+        presenter?.viewDidLoad()
     }
     
-
+    func setupUI() {
+        filesTableView.register(FileTableViewCell.self)
+    }
 }
 
 extension ListFilesViewController: ListFilesView {
+    
+}
+
+extension ListFilesViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return presenter?.numberOfFiles() ?? 0
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let file = presenter?.getFile(at: indexPath.row) else {return UITableViewCell()}
+        let cell = tableView.dequeueCell() as FileTableViewCell
+        cell.audioDurationLabel.text = file.fileName
+        cell.fileNameLabel.text = file.fileName
+        cell.audioDurationLabel.text = file.duration
+        if file.fileType == .Folder {
+            cell.fileImageView.image = UIImage(systemName: "folder.fill")
+            cell.fileImageView.tintColor = UIColor(named: "Folder")
+            cell.audioDurationLabel.isHidden = true
+        } else {
+            cell.fileImageView.image = UIImage(systemName: "music.note")
+            cell.fileImageView.tintColor = UIColor(named: "Audio")
+        }
+        return cell
+    }
+    
     
 }
