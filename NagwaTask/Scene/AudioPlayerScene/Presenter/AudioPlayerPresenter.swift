12df7b2c4dde7c4a2f2c3f280c4047160f64aff1
ZyadGalal <dev_zyad_galal@yahoo.com>
2022-03-29 01:47:25 +0200

    implement play , pause audio . setup (slider , remaining label, elapsed label ) to be same as audio progress , add ability to  scrub through audio from slider

--- NagwaTask/Scene/AudioPlayerScene/Presenter/AudioPlayerPresenter.swift
+++ NagwaTask/Scene/AudioPlayerScene/Presenter/AudioPlayerPresenter.swift
@@ -7,27 +7,50 @@
 
 
 import Foundation
+import AVFoundation
 
 protocol AudioPlayerView: AnyObject{
     var presenter: AudioPlayerPresenterProtocol? {get set}
-    func updateUIWith(audioName: String)
+
 }
 
 protocol AudioPlayerPresenterProtocol: AnyObject {
+    var path: URL? {get}
     func viewDidLoad()
+    func getStringTime(from timeInterval: TimeInterval) -> String
+    func getAudioName() -> String 
 }
 
 class AudioPlayerPresenter: AudioPlayerPresenterProtocol{
-    
-    private let path: URL?
+
+    var path: URL?
     private weak var view: AudioPlayerView?
     private var router: AudioPlayerRouter
+    private let formatter: DateComponentsFormatter = {
+        let formatter = DateComponentsFormatter()
+        formatter.unitsStyle = .positional
+        formatter.zeroFormattingBehavior = .pad
+        formatter.allowedUnits = [.hour, .minute, .second]
+        return formatter
+    }()
+
     init (view: AudioPlayerView, router: AudioPlayerRouter, path: URL){
         self.view = view
         self.router = router
         self.path = path
     }
     func viewDidLoad() {
-        self.view?.updateUIWith(audioName: path?.deletingPathExtension().lastPathComponent ?? "")
+        
     }
+
+    
+    func getStringTime(from timeInterval: TimeInterval) -> String{
+        return formatter.string(from: timeInterval) ?? "00:00"
+    }
+
+    func getAudioName() -> String {
+        return path?.deletingPathExtension().lastPathComponent ?? ""
+    }
+    
+
 }
--- NagwaTask/Scene/AudioPlayerScene/View/AudioPlayerViewController.swift
+++ NagwaTask/Scene/AudioPlayerScene/View/AudioPlayerViewController.swift
@@ -6,21 +6,38 @@
 //
 
 import UIKit
+import AVFoundation
 
 class AudioPlayerViewController: UIViewController {
 
+    @IBOutlet weak var elapsedTimeLabel: UILabel!
+    @IBOutlet weak var remainingTimeLabel: UILabel!
     @IBOutlet weak var forwardButton: UIButton!
     @IBOutlet weak var playButton: UIButton!
     @IBOutlet weak var backwardButton: UIButton!
     @IBOutlet weak var audioSlider: UISlider!
     @IBOutlet weak var audioLabel: UILabel!
     
+    private var player: AVAudioPlayer?
+    private var timer: Timer?
+
+    
     var presenter: AudioPlayerPresenterProtocol?
     override func viewDidLoad() {
         super.viewDidLoad()
         presenter?.viewDidLoad()
+        setupPlayer()
+        setupSlider()
+        setupUI()
+        togglePlay()
     }
+    override func viewWillDisappear(_ animated: Bool) {
+        timer?.invalidate()
+    }
+
+
     @IBAction func playButtonDidClicked(_ sender: Any) {
+        togglePlay()
     }
     
     @IBAction func forwardButtonDidClicked(_ sender: Any) {
@@ -28,12 +45,72 @@ class AudioPlayerViewController: UIViewController {
     
     @IBAction func backwardButtonDidClicked(_ sender: Any) {
     }
+    
+    @IBAction func SliderValueChanged(_ sender: UISlider) {
+        player?.currentTime = TimeInterval(sender.value)
+    }
+    func setupUI() {
+        guard let duration = player?.duration, let currentTime = player?.currentTime else{return }
+        audioLabel.text = presenter?.getAudioName()
+        elapsedTimeLabel.text = presenter?.getStringTime(from: currentTime)
+        remainingTimeLabel.text = presenter?.getStringTime(from: duration)
+    }
+    func setupSlider() {
+        guard let duration = player?.duration else {return}
+        audioSlider.maximumValue = Float(duration)
+        audioSlider.value = 0.0
+    }
+    private func setupPlayer() {
+        do {
+            guard let path = presenter?.path else {return}
+            player = try AVAudioPlayer(contentsOf: path)
+            player?.delegate = self
+        } catch {
+            print(error)
+        }
+    }
+    private func play() {
+        guard let player = player else {return}
+        player.prepareToPlay()
+        player.volume = 1.0
+        timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(self.updateTime(_:)), userInfo: nil, repeats: true)
+        player.play()
+        
+    }
+    @objc private func updateTime(_ timer: Timer) {
+        guard let player = player else {return}
+        let remainingTime = player.duration - player.currentTime
+        let elapsedTime = player.currentTime
+        audioSlider.value = Float(player.currentTime)
+        remainingTimeLabel.text = presenter?.getStringTime(from: remainingTime)
+        elapsedTimeLabel.text = presenter?.getStringTime(from: elapsedTime)
+    
+    }
+
+    func togglePlay() {
+        guard let player = player else {return}
+        if player.isPlaying {
+            player.pause()
+            timer?.invalidate()
+            playButton.setImage(UIImage(systemName: "play.fill"), for: .normal)
+        } else {
+            self.play()
+            playButton.setImage(UIImage(systemName: "pause.fill"), for: .normal)
+        }
+    }
+
 }
 
 extension AudioPlayerViewController: AudioPlayerView {
-    func updateUIWith(audioName: String) {
-        self.audioLabel.text = audioName
+    
+}
+
+
+extension AudioPlayerViewController: AVAudioPlayerDelegate {
+    func audioPlayerDidFinishPlaying(_ player: AVAudioPlayer, successfully flag: Bool) {
+        if flag {
+            timer?.invalidate()
+            playButton.setImage(UIImage(systemName: "play.fill"), for: .normal)
+        }
     }
-    
-    
 }
